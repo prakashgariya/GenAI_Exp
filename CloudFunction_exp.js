dc0f0ec84c72d253d87b0325bcb26d50231a126a
prakashgariya <143708519+prakashgariya@users.noreply.github.com>
2023-09-06 17:06:00 +0530

    Add files via upload

---
+++ CloudFunction_exp.js
@@ -0,0 +1,67 @@
+(function () {
+
+    const jQueryScript = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js";
+    let template = document.createElement("template");
+    template.innerHTML = `
+    <style>
+    .button {
+        background-color: #4CAF50;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        width:100%;
+    }
+    .textarea {
+        width:100%;
+        height:514px;
+    }
+    </style>
+    <div>
+    <button class="button">Generate Insights</button>
+    <textarea id="textArea" name="textArea" class="textarea">Generated Insights will show up here</textarea>
+    </div>
+    `;
+
+    class CloudFunction extends HTMLElement {
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({ mode: "open" });
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
+
+            this._button = this._shadowRoot.querySelector('button');
+            this._button.addEventListener('click', this._onButtonClick.bind(this));
+
+        }
+
+        _onButtonClick(event) {
+            let sToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjgzOGMwNmM2MjA0NmMyZDk0OGFmZmUxMzdkZDUzMTAxMjlmNGQ1ZDEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTE0Mzc4MzE0ODMyNjg4MzIxNzgwIiwiaGQiOiJkZWxvaXR0ZS5jb20iLCJlbWFpbCI6InVzYS1wZ2FyaXlhQGRlbG9pdHRlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiTjE1eFhzdHdlYk9YZG5zVzFQUlZtZyIsIm5iZiI6MTY5Mzk5ODAzNSwiaWF0IjoxNjkzOTk4MzM1LCJleHAiOjE2OTQwMDE5MzUsImp0aSI6IjQyNTM1NDA1MTQ5OGE4MDE1NjAxZGYzYTdkZTg1MzQyNTAxYTA0YjMifQ.eCk1Az1jCIVuQYkBDnwz99joMPo_ZwWyPjHNX2muhCdGWwI9BB5heu4kqHxYZgIkHAkDqfgY3mtYDaS3diC5GQhFxFJ-qb46zobywOn7zAhBZ2Upehx4knMpBmurQFF9xxgdMk80hTYX6sKHDFolZmLzbIOFStVlE1hMSQMXbgBCLfU3cY4o9mNowfra43EiMXDUBIq9UfRDa1aCYMOWUwJeRnot6dx2epCzHuY9l9YzE6NtX-pX-GjTUm_3_CnPOj-KK6NAJo1RPsp-rEGWnpGKc5Lc9wTRICr-PCQYG3s8X0jHxgOuZs_pzrsGcfMbxWKL3Vbh0ZV1NQy-Nctgaw";
+            var textArea = this.shadowRoot.getElementById('textArea');
+            textArea.value = "Fetching Insights....";
+            jQuery.ajax({
+                url: "https://us-central1-us-gcp-ame-con-e74c9-sbx-1.cloudfunctions.net/GCF_Gen_Analytics_trigger",
+                type: "GET",
+                crossDomain: true,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', 'Bearer ' + sToken);
+                },
+                crossDomain: true,
+                error: function (err) {
+                    console.log("Error");
+                    textArea.value = err.responseText;
+                },
+                success: function (data, textStatus) {
+                    console.log("Success");
+                    textArea.value = data;
+                }
+            });
+        }
+    }
+
+    customElements.define('sac-cloudfunction', CloudFunction);
+})();
