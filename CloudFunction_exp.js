78fb86385d61495ab4deb2fcb75f8a89116123fc
prakashgariya <143708519+prakashgariya@users.noreply.github.com>
2023-09-06 18:08:36 +0530

    Add files via upload

--- CloudFunction_exp.js
+++ CloudFunction_exp.js
@@ -20,10 +20,12 @@
     .textarea {
         width:100%;
         height:514px;
+        background-color: aqua;
     }
     </style>
     <div>
     <button class="button">Generate Insights</button>
+    <img src="https://prakashgariya.github.io/GenAI_Exp/BA.png" alt="Finance Analyst"/>
     <textarea id="textArea" name="textArea" class="textarea">Generated Insights will show up here</textarea>
     </div>
     `;
@@ -37,11 +39,16 @@
             this._button = this._shadowRoot.querySelector('button');
             this._button.addEventListener('click', this._onButtonClick.bind(this));
 
+            this._shadowRoot.querySelector('textArea').hidden = true;
+
         }
 
         _onButtonClick(event) {
             let sToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjgzOGMwNmM2MjA0NmMyZDk0OGFmZmUxMzdkZDUzMTAxMjlmNGQ1ZDEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTE0Mzc4MzE0ODMyNjg4MzIxNzgwIiwiaGQiOiJkZWxvaXR0ZS5jb20iLCJlbWFpbCI6InVzYS1wZ2FyaXlhQGRlbG9pdHRlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiTjE1eFhzdHdlYk9YZG5zVzFQUlZtZyIsIm5iZiI6MTY5Mzk5ODAzNSwiaWF0IjoxNjkzOTk4MzM1LCJleHAiOjE2OTQwMDE5MzUsImp0aSI6IjQyNTM1NDA1MTQ5OGE4MDE1NjAxZGYzYTdkZTg1MzQyNTAxYTA0YjMifQ.eCk1Az1jCIVuQYkBDnwz99joMPo_ZwWyPjHNX2muhCdGWwI9BB5heu4kqHxYZgIkHAkDqfgY3mtYDaS3diC5GQhFxFJ-qb46zobywOn7zAhBZ2Upehx4knMpBmurQFF9xxgdMk80hTYX6sKHDFolZmLzbIOFStVlE1hMSQMXbgBCLfU3cY4o9mNowfra43EiMXDUBIq9UfRDa1aCYMOWUwJeRnot6dx2epCzHuY9l9YzE6NtX-pX-GjTUm_3_CnPOj-KK6NAJo1RPsp-rEGWnpGKc5Lc9wTRICr-PCQYG3s8X0jHxgOuZs_pzrsGcfMbxWKL3Vbh0ZV1NQy-Nctgaw";
             var textArea = this.shadowRoot.getElementById('textArea');
+            var image = this.shadowRoot.getElementById('image');
+            image.hidden = true;
+            textArea.hidden = false;
             textArea.value = "Fetching Insights....";
             jQuery.ajax({
                 url: "https://us-central1-us-gcp-ame-con-e74c9-sbx-1.cloudfunctions.net/GCF_Gen_Analytics_trigger",
